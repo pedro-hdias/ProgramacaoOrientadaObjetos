860df20595bee705924436c009f1c47a5a918fd6
pedro-hdias <83011148+pedro-hdias@users.noreply.github.com>
2021-11-30 12:56:32 -0300

    Início da oriemntação aos objetos.

---
+++ Pessoa.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ProgramacaoOrientadaObjetos
+{
+    public class Pessoa
+    {
+        public int Codigo { get; set; }
+
+        public string? nome { get; set; }
+        public string? sobrenome { get; set; }
+
+        public DateTime dataNascimento { get; set; }
+    }
+}
